b78804184bbb5b51f6179cd6561664d5be4ae55c
sranso <sranso@gmail.com>
2014-03-27 13:54:51 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OutsideInBddArtPrototype::Application.config.secret_key_base = 'a19b57bd8810cbc3a047ec2623ab94579c140ec93edf624d4f46308f6f713a04a8921bf84714ef88d89effca583d445d57ba6f1c09baeb606eda1bc435a5a709'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OutsideInBddArtPrototype::Application.config.session_store :cookie_store, key: '_outside-in-bdd-art-prototype_session'
